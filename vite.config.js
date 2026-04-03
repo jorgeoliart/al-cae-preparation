6fc40353a6f575b4f2578520939a6edb8ff3eb6c
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2026-04-03 08:24:07 -0500

    Add html for week 2 session s2

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         caeS2W1: resolve(__dirname, 'cae_s2_w1_passive_constructions.html'),
         caeS3W1: resolve(__dirname, 'cae_s3_w1_passive_constructions.html'),
         caeS1W2: resolve(__dirname, 'cae_s1_w2_modals.html'),
+        caeS2W2: resolve(__dirname, 'cae_s2_w2_modals.html'),
         // Add new session entries here
       }
     }
