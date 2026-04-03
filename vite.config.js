ee8521fa3dd577b74dfe3805191bca4b8cbce210
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2026-04-03 18:47:28 -0500

    Replace week 1 session s4 with reading html

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         caeS1W1: resolve(__dirname, 'cae_s1_w1_passive_constructions.html'),
         caeS2W1: resolve(__dirname, 'cae_s2_w1_passive_constructions.html'),
         caeS3W1: resolve(__dirname, 'cae_s3_w1_reading.html'),
+        caeS4W1: resolve(__dirname, 'cae_s4_w1_reading.html'),
         caeS1W2: resolve(__dirname, 'cae_s1_w2_modals.html'),
         caeS2W2: resolve(__dirname, 'cae_s2_w2_modals.html'),
         caeS3W2: resolve(__dirname, 'cae_s3_w2_modals.html'),
