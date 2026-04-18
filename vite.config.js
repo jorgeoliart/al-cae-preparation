75974b33e63c50cb504246aa0d7d59ee64e780ef
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2026-04-18 12:49:18 -0500

    Add listening part 4 for week 3

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,8 @@ export default defineConfig({
         caeS3W2: resolve(__dirname, 'cae_s3_w2_reading.html'),
         caeW3S2: resolve(__dirname, 'cae_listening_p1_w3_01.html'),
         listeningP1W3: resolve(__dirname, 'cae_listening_p1_w3_01.html'),
+        caeW3S4L1: resolve(__dirname, 'cae_listening_p4_w3_01.html'),
+        listeningP4W3: resolve(__dirname, 'cae_listening_p4_w3_01.mp3'),
         // Add new session entries here
       }
     }
